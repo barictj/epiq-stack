ca4d128c9c471450839d664abd461aae8fe92ffd
Tomm <barictj@gmail.com>
2025-08-30 17:19:22 -0500

    Changes

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,23 +1,36 @@
 import React from 'react';
+import PlayerCard from '@components/PlayerCard/PlayerCard';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 export async function getServerSideProps() {
     const res = await fetch('http://backend:3001/api/items');
     const items = await res.json();
-    console.log(items)
+
     return {
         props: { items },
     };
 }
 
 export default function Home({ items }: { items: any[] }) {
+    console.log('mainpage    ' + items)
     return (
-        <div>
-            <h1>Items</h1>
-            <ul>
-                {items.map(item => (
-                    <li key={item.id}>{item.name}</li>
-                ))}
-            </ul>
-        </div>
+        <Container>
+            {items.map((item) => (
+                <>
+                    <Row>
+                        <Col xs={9} offset={4}> <PlayerCard player={item} /></Col>
+                        <Col > <Container><PlayerCard player={item} /></Container></Col>
+                    </Row>
+                    <Row>
+                        <Col>1 of 3</Col>
+                        <Col>2 of 3</Col>
+                        <Col>3 of 3</Col>
+                    </Row>
+                </>
+            ))}
+
+        </Container>
     );
 }
